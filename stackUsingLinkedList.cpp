cda75ec55ca1a5569b5048c6b05289746ad58df4
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-05-20 10:27:13 +0700

    menambahkan fungsi push

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -17,5 +17,19 @@ class Stack
 {
 private : 
     Node *top;
+public :
+    Stack()
+    {
+        top = NULL;
+    }
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << " Push Value : " << value << endl;
+        return value;
+    }
 
 };
